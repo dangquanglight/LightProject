b71dd3f5ed838674887564d2dbc1fb033284a601
dangquanglight <13141992>
2014-06-28 08:31:27 +0700

    ...

--- gehouse.sql
+++ gehouse.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.6
+-- version 4.1.12
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 25, 2014 at 10:51 AM
+-- Generation Time: Jun 28, 2014 at 03:25 AM
 -- Server version: 5.6.16
--- PHP Version: 5.5.9
+-- PHP Version: 5.5.11
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -101,6 +101,22 @@ CREATE TABLE IF NOT EXISTS `buildings` (
 
 INSERT INTO `buildings` (`id`, `building_name`, `description`, `status`, `created_date`, `created_by`) VALUES
 (1, 'building 1', NULL, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ci_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `ci_sessions` (
+  `session_id` varchar(40) NOT NULL DEFAULT '0',
+  `ip_address` varchar(16) NOT NULL DEFAULT '0',
+  `user_agent` varchar(120) DEFAULT NULL,
+  `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
+  `user_data` text NOT NULL,
+  PRIMARY KEY (`session_id`),
+  KEY `last_activity` (`last_activity`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -217,7 +233,7 @@ CREATE TABLE IF NOT EXISTS `device_setpoints` (
 INSERT INTO `device_setpoints` (`id`, `row_device_id`, `value`) VALUES
 (1, 1, 20),
 (6, 10, 38),
-(8, 11, 15),
+(8, 11, 17),
 (9, 12, 25),
 (12, 9, 255),
 (13, 8, 56),
@@ -1544,6 +1560,126 @@ INSERT INTO `units` (`id`, `unit_name`, `description`, `created_date`, `created_
 (10, 'kWh', 'kWh', 1, 1),
 (11, '%', 'percent', 1, 1),
 (12, 'volt', 'Volt', 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_accounts`
+--
+
+CREATE TABLE IF NOT EXISTS `user_accounts` (
+  `uacc_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `uacc_group_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `uacc_email` varchar(100) NOT NULL DEFAULT '',
+  `uacc_username` varchar(15) NOT NULL DEFAULT '',
+  `uacc_password` varchar(60) NOT NULL DEFAULT '',
+  `uacc_ip_address` varchar(40) NOT NULL DEFAULT '',
+  `uacc_salt` varchar(40) NOT NULL DEFAULT '',
+  `uacc_activation_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_forgotten_password_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_forgotten_password_expire` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `uacc_update_email_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_update_email` varchar(100) NOT NULL DEFAULT '',
+  `uacc_active` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `uacc_suspend` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `uacc_fail_login_attempts` smallint(5) NOT NULL DEFAULT '0',
+  `uacc_fail_login_ip_address` varchar(40) NOT NULL DEFAULT '',
+  `uacc_date_fail_login_ban` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Time user is banned until due to repeated failed logins',
+  `uacc_date_last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `uacc_date_added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`uacc_id`),
+  UNIQUE KEY `uacc_id` (`uacc_id`),
+  KEY `uacc_group_fk` (`uacc_group_fk`),
+  KEY `uacc_email` (`uacc_email`),
+  KEY `uacc_username` (`uacc_username`),
+  KEY `uacc_fail_login_ip_address` (`uacc_fail_login_ip_address`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `user_groups` (
+  `ugrp_id` smallint(5) NOT NULL AUTO_INCREMENT,
+  `ugrp_name` varchar(20) NOT NULL DEFAULT '',
+  `ugrp_desc` varchar(100) NOT NULL DEFAULT '',
+  `ugrp_admin` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`ugrp_id`),
+  UNIQUE KEY `ugrp_id` (`ugrp_id`) USING BTREE
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `user_groups`
+--
+
+INSERT INTO `user_groups` (`ugrp_id`, `ugrp_name`, `ugrp_desc`, `ugrp_admin`) VALUES
+(1, 'Root admin', 'Root Admin : has full admin access rights.', 1),
+(2, 'Buildings owner', 'Buildings owner: has full admin access in whole building rights', 1),
+(3, 'Rooms admin', 'Rooms admin: has full admin access in whole room rights', 1),
+(4, 'Public', 'Public User : has no admin access rights.', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_login_sessions`
+--
+
+CREATE TABLE IF NOT EXISTS `user_login_sessions` (
+  `usess_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `usess_series` varchar(40) NOT NULL DEFAULT '',
+  `usess_token` varchar(40) NOT NULL DEFAULT '',
+  `usess_login_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`usess_token`),
+  UNIQUE KEY `usess_token` (`usess_token`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_privileges`
+--
+
+CREATE TABLE IF NOT EXISTS `user_privileges` (
+  `upriv_id` smallint(5) NOT NULL AUTO_INCREMENT,
+  `upriv_name` varchar(20) NOT NULL DEFAULT '',
+  `upriv_desc` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`upriv_id`),
+  UNIQUE KEY `upriv_id` (`upriv_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_privilege_groups`
+--
+
+CREATE TABLE IF NOT EXISTS `user_privilege_groups` (
+  `upriv_groups_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
+  `upriv_groups_ugrp_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `upriv_groups_upriv_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`upriv_groups_id`),
+  UNIQUE KEY `upriv_groups_id` (`upriv_groups_id`) USING BTREE,
+  KEY `upriv_groups_ugrp_fk` (`upriv_groups_ugrp_fk`),
+  KEY `upriv_groups_upriv_fk` (`upriv_groups_upriv_fk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_privilege_users`
+--
+
+CREATE TABLE IF NOT EXISTS `user_privilege_users` (
+  `upriv_users_id` smallint(5) NOT NULL AUTO_INCREMENT,
+  `upriv_users_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `upriv_users_upriv_fk` smallint(5) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`upriv_users_id`),
+  UNIQUE KEY `upriv_users_id` (`upriv_users_id`) USING BTREE,
+  KEY `upriv_users_uacc_fk` (`upriv_users_uacc_fk`),
+  KEY `upriv_users_upriv_fk` (`upriv_users_upriv_fk`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
